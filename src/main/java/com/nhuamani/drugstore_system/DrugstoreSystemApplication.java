e0b1a47afc62666cce82166b15a017622316686b
nhuamani <nhuamani.c7@gmail.com>
2026-07-06 16:27:15 -0500

    Iniciación del proyecto

---
+++ src/main/java/com/nhuamani/drugstore_system/DrugstoreSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nhuamani.drugstore_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugstoreSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugstoreSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhuamani/drugstore_system/DrugstoreSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhuamani.drugstore_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugstoreSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
