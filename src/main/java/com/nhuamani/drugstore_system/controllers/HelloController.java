b24fc02006cd929a9705f3035e71e14b675d8bf9
nhuamani <nhuamani.c7@gmail.com>
2026-07-06 16:58:23 -0500

    Se agrega model

--- src/main/java/com/nhuamani/drugstore_system/controllers/HelloController.java
+++ src/main/java/com/nhuamani/drugstore_system/controllers/HelloController.java
@@ -1,12 +1,14 @@
 package com.nhuamani.drugstore_system.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
     @GetMapping("/hello")
-    public String sayHello() {
+    public String sayHello(Model model) {
+        model.addAttribute("message", "Hello, World! with Thymeleaf in Spring MVC Model");
         return "hello";
     }
 }
