73539821afd8f1b50ffe5e8270148a0dde5410f3
nhuamani <nhuamani.c7@gmail.com>
2026-08-10 11:55:24 -0500

    update RoleRepository

--- src/main/java/com/nhuamani/drugstore_system/repositories/RoleRepository.java
+++ src/main/java/com/nhuamani/drugstore_system/repositories/RoleRepository.java
@@ -3,10 +3,12 @@ package com.nhuamani.drugstore_system.repositories;
 import com.nhuamani.drugstore_system.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    Role findByName(String name);
+    Optional<Role> findByName(String name);
 
-    Role findByDescription(String description);
+    boolean existsByName(String name);
 
 }
